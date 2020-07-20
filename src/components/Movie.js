62935fb69f3bb2b95e259840591aabd5e1dbedf6
gladys-pascual <gladyskate@gmail.com>
2020-07-20 22:10:47 +0100

    movie and tv routing when you click on cards using class for movie and hooks for tv

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 
 class Movie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  fetchMovie = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          movie: response,
+          loading: false,
+        });
+      })
+      .catch((err) => console.log("Error fetching and parsing data", err));
+  };
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    console.log(this.state.movie);
     return (
       <>
         <p>Movie component</p>
--- src/components/Tv.js
+++ src/components/Tv.js
@@ -1,13 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-class Tv extends Component {
-  render() {
-    return (
-      <>
-        <p>TV component</p>
-      </>
-    );
-  }
-}
+const Tv = (props) => {
+  const [tv, setTv] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setTv(response);
+        setLoading(false);
+      })
+      .catch((err) => console.log("Error fetching and parsing data", err));
+  }, [id]);
+
+  return (
+    <>
+      <p>TV component</p>
+    </>
+  );
+};
 
 export default Tv;
